6f43d63fe8706b9becebc70e05a625ee73b784b8
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-01 14:18:41 +0400

    Create component search form

--- my-app/src/components/SearchForm.jsx
+++ my-app/src/components/SearchForm.jsx
@@ -1,2 +1,30 @@
 import React from 'react';
-import { Form, useSubmit } from 'react-router-dom'; 
+import { Form, useSubmit } from 'react-router-dom';
+
+const SearchForm = ({ searching, q }) => {
+  const submit = useSubmit();
+
+  return (
+    <Form id="search-form" role="search">
+      <input
+        id="q"
+        className={searching ? 'loading' : ''}
+        aria-label="Search tasks"
+        placeholder="Search"
+        type="search"
+        name="q"
+        defaultValue={q}
+        onChange={(event) => {
+          const isFirstSearch = q == null;
+          submit(event.currentTarget.form, {
+            replace: !isFirstSearch,
+          });
+        }}
+      />
+      <div id="search-spinner" aria-hidden hidden={!searching} />
+      <div className="sr-only" aria-live="polite"></div>
+    </Form>
+  );
+};
+
+export default SearchForm;
