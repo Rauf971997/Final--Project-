9e5bc3718fd47ed98df05755ce0b122984c8c2f0
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-01 11:12:12 +0400

    Create Browser Router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,3 +7,38 @@ import ErrorPage from "./routes/error-page";
 import EditTask, { action as editAction } from "./routes/EditPage";
 import TaskDetail, { loader as taskDetailLoader } from "./routes/TaskDetail";
 import { action as destroyAction } from "./routes/destroy";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainPage />,
+      errorElement: <ErrorPage />,
+      loader: mainLoader,
+      action: mainAction,
+      children: [
+        {
+          path: "task/:taskId",
+          element: <TaskDetail />,
+          loader: taskDetailLoader,
+        },
+        {
+          path: "task/:taskId/edit",
+          element: <EditTask />,
+          loader: taskDetailLoader,
+          action: editAction,
+        },
+  
+        {
+          path: "task/:taskId/destroy",
+          action: destroyAction,
+        },
+      ],
+    },
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+  
