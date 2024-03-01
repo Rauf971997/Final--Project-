a1f59497e2d68971912e818977e7ee038f3e7984
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-01 13:57:41 +0400

    Define function for task details which return title and description

--- my-app/src/routes/TaskDetail.jsx
+++ my-app/src/routes/TaskDetail.jsx
@@ -5,3 +5,16 @@ export async function loader({ params }) {
   const task = await getTask(params.taskId);
   return { task };
 }
+
+export default function TaskDetail() {
+    const { task } = useLoaderData();
+    return (
+      <div id="task-details">
+        <div>
+          <p>Title: {task.title}</p>
+  
+          <p>Description:{task.description}</p>
+        </div>
+      </div>
+    );
+  }
