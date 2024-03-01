17cd08aecbde5949c7ed0b616b1833d349199a57
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-01 13:51:15 +0400

    Create function and sent components

--- my-app/src/routes/MainPage.jsx
+++ my-app/src/routes/MainPage.jsx
@@ -19,3 +19,77 @@ export async function loader({ request }) {
     const tasks = await getTasks(q);
     return { tasks, q };
   }
+
+  export default function MainPage() {
+    const { tasks: initialTasks, q } = useLoaderData();
+    const navigation = useNavigation();
+  
+    const [tasks, setTasks] = useState(initialTasks);
+    const [searching] = useState(
+      navigation.location &&
+        new URLSearchParams(navigation.location.search).has("q")
+    );
+    const [filter, setFilter] = useState("all");
+  
+    useEffect(() => {
+      document.getElementById("q").value = q;
+    }, [q]);
+  
+    useEffect(() => {
+      setTasks(initialTasks);
+    }, [initialTasks]);
+  
+    const handleFilter = (filterType) => {
+      setFilter(filterType);
+    };
+  
+    const handleUpdateTaskStatus = async (taskId, isCompleted) => {
+      try {
+        await updateTask(taskId, { completed: isCompleted });
+        const updatedTasks = tasks.map((task) =>
+          task.id === taskId ? { ...task, completed: isCompleted } : task
+        );
+        setTasks(updatedTasks);
+      } catch (error) {
+        console.error("Error updating task status:", error);
+      }
+    };
+  
+    return (
+      <>
+        <div id="sidebar">
+          <h1>TO-DO LIST</h1>
+          <div>
+            <SearchForm searching={searching} q={q} />
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <FilterButtons handleFilter={handleFilter} filter={filter} />
+  
+          {tasks.length ? (
+            <TaskList
+              tasks={tasks.filter((task) => {
+                if (filter === "all") return true;
+                if (filter === "completed") return task.completed;
+                if (filter === "uncompleted") return !task.completed;
+                return false;
+              })}
+              handleUpdateTaskStatus={handleUpdateTaskStatus}
+            />
+          ) : (
+            <p>
+              <i>No Tasks</i>
+            </p>
+          )}
+        </div>
+  
+        <div
+          id="detail"
+          className={navigation.state === "loading" ? "loading" : ""}
+        >
+          <Outlet />
+        </div>
+      </>
+    );
+  }
