df348297df04720be84e6190b7b8289be0565c2e
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-04 13:18:52 +0400

    Changed styles for details page and add button

--- my-app/src/routes/TaskDetail.jsx
+++ my-app/src/routes/TaskDetail.jsx
@@ -7,14 +7,20 @@ export async function loader({ params }) {
 }
 
 export default function TaskDetail() {
-    const { task } = useLoaderData();
-    return (
-      <div id="task-details">
-        <div>
-          <p>Title: {task.title}</p>
-  
-          <p>Description:{task.description}</p>
+  const { task } = useLoaderData();
+  return (
+    <>
+      <h2>Task Details</h2>
+      <div id="task-details" className="task-details-container">
+        <div className="task-details-content">
+          <p className="task-details-title">
+            Title: <span className="task-title">{task.title}</span>
+          </p>
+          <p className="task-details-description">
+            Description: <span className="task-description">{task.description}</span>
+          </p>
         </div>
       </div>
-    );
-  }
+    </>
+  );
+}
